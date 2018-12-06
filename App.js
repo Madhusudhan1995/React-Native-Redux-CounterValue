4a219aa83c9597110418c3983bcb7d0b3387fb54
Madhusudhan1995 <44834708+Madhusudhan1995@users.noreply.github.com>
2018-12-06 20:17:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,43 +7,41 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import CounterValue from './src/reducers/CounterValue';
 
-type Props = {};
-export default class App extends Component<Props> {
+const initialState ={
+  counter:0
+}
+const reducer = (state = initialState,action) => {
+  switch(action.type){
+    case 'INCREASE_COUNTER':
+      return{counter:state.counter+1}
+    case 'DECREASE_COUNTER':
+      return{counter:state.counter-1}
+   
+  }
+    return state
+}
+const store = createStore(reducer)
+
+export default class App extends Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+       <Provider style={styles.container} store={store}>
+           <CounterValue />
+       </Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  container:{
+    justifyContent:'center',
+    alignItems:'center',
+    flex:1
+  }
+})
